1c9adc54f13292fd4b3905440e0e06ff297fd620
Raghav0422 <60313641+Raghav0422@users.noreply.github.com>
2023-11-30 06:08:33 +0000

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component{
   render(){
     return(
       <div>
         This is news component
+        <NewsItem />
       </div>
     )
   }
