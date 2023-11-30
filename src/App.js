e6aeeba2e78cb60d37c717c41800bc25c5d9b437
Raghav0422 <60313641+Raghav0422@users.noreply.github.com>
2023-11-30 06:00:58 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React, {Component} from "react";
+import NavBar from '.components/NavBar;
 
 export default class App extends Component{
-  c= "Raghav";
+  
   render(){
     return(
       <div>
-        Hello my first class based component {this.c}
+        <NavBar />
       </div>
     )
   }
