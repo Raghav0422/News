f0d076d0593777469de11a222d3712939095d565
Raghav0422 <60313641+Raghav0422@users.noreply.github.com>
2023-11-30 05:42:52 +0000

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,18 @@
+import React, {Component} from 'react'
+import PropTypes, from 'prop-types'
 
+export class NavBar extends Component{
+  static propTypes={
+
+  }
+
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Navbar
