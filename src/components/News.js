e004175d79c3c65c857f0a4c2a93937954ec0693
Raghav0422 <60313641+Raghav0422@users.noreply.github.com>
2023-11-30 06:02:43 +0000

    Create News.js

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+export default class News extends Component{
+  render(){
+    return(
+      <div>
+        This is news component
+      </div>
+    )
+  }
+}
+
+export default News
